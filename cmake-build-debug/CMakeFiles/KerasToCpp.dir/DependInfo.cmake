5472653af2885c0bcd0d8c241dab78b0e94695d3
Alexandru-Marius Solomes <alexandrusoloms@localhost.localdomain>
2019-10-21 20:24:23 +0100

    binary

---
+++ cmake-build-debug/CMakeFiles/KerasToCpp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alexandrusoloms/bad/KerasToCpp/core/Convolution2D.cpp" "/home/alexandrusoloms/bad/KerasToCpp/cmake-build-debug/CMakeFiles/KerasToCpp.dir/core/Convolution2D.cpp.o"
+  "/home/alexandrusoloms/bad/KerasToCpp/core/MaxPooling.cpp" "/home/alexandrusoloms/bad/KerasToCpp/cmake-build-debug/CMakeFiles/KerasToCpp.dir/core/MaxPooling.cpp.o"
+  "/home/alexandrusoloms/bad/KerasToCpp/core/array4d.cpp" "/home/alexandrusoloms/bad/KerasToCpp/cmake-build-debug/CMakeFiles/KerasToCpp.dir/core/array4d.cpp.o"
+  "/home/alexandrusoloms/bad/KerasToCpp/src/main.cpp" "/home/alexandrusoloms/bad/KerasToCpp/cmake-build-debug/CMakeFiles/KerasToCpp.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
